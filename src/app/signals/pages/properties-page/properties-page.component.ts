e180e41f35e9640e123080af351c4e342669ed73
Javier  Restrepo Toro <jarestrepot@uqvirtual.edu.co>
2024-01-28 20:05:08 +0100

    Signal mutate

--- src/app/signals/pages/properties-page/properties-page.component.ts
+++ src/app/signals/pages/properties-page/properties-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+import { User } from '../../interfaces/users-request.interface';
 
 @Component({
   templateUrl: './properties-page.component.html',
@@ -6,4 +7,56 @@ import { Component } from '@angular/core';
 })
 export class PropertiesPageComponent {
 
+  public user = signal<User>({
+    id: 1,
+    email: "george.bluth@reqres.in",
+    first_name: "George",
+    last_name: "Bluth",
+    avatar: "https://reqres.in/img/faces/1-image.jpg"
+  });
+
+  public fullName = computed( () => `${ this.user().first_name } ${ this.user().last_name }`)
+
+  // keyof User only keys for user interface
+  onFiedlUpdated( field: keyof User , value: string ):void {
+
+    // this.user.set( {
+    //   ...this.user(),
+    //   [field]: value
+    // });
+
+    // Actualizando
+    // this.user.update( currentUser => ({
+    //   ...currentUser,
+    //   [field]: value
+    // }));
+
+
+    this.user.update( currentUser => {
+      // Mutate
+      switch( field ) {
+        case 'email': {
+          currentUser.email = value;
+          break;
+        }
+        case 'first_name': {
+          currentUser.first_name = value;
+          break;
+        }
+        case 'last_name': {
+          currentUser.last_name = value;
+          break;
+        }
+        case 'avatar': {
+          currentUser.avatar = value;
+          break;
+        }
+        case 'id': {
+          currentUser.id = Number( value );
+          break;
+        }
+      }
+      return currentUser
+    })
+  }
 }
